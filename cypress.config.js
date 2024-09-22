e8d0024e4e98a3f3c5398d282d7c69fa89e337d6
Anderson Silva <andersonsilvadev01@gmail.com>
2024-09-22 16:12:18 -0300

    funcionalidade login

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    viewportWidth: 1280,
-    viewportHeight: 720,
+    // viewportWidth: 1000,
+    // viewportHeight: 800,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/01_carrinho.cy.js
+++ cypress/e2e/01_carrinho.cy.js
@@ -1,17 +1,17 @@
 /// <reference types='cypress' />
 
 
-describe('Adicionar item ao carrinho', () => {
+describe('Funcionalidade carrinho', () => {
     beforeEach(() => {
 cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/');
 });
-it('Deve adicionar 10 itens ao carrinho com sucesso', () => {
+it('Deve adicionar um item ao carrinho com sucesso', () => {
     cy.login('aluno_ebac@teste.com', 'teste@teste.com')
     cy.get('#primary-menu > .menu-item-629 > [href="http://lojaebac.ebaconline.art.br/produtos/"]').click()
     cy.get('.post-3073 > .product-block > .block-inner > .image > .product-image > .image-hover').click()
     cy.get('.button-variable-item-36').click()
     cy.get('.button-variable-item-Green').click()
-    cy.get('.input-text').clear().type('10')
+    cy.get('.input-text').clear().type('1')
     cy.get('.single_add_to_cart_button').click();
     cy.get('.dropdown-toggle > .text-skin > .icon-basket').click()
     cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click()
