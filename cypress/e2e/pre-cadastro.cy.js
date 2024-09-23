5f287522583ce7b80acb1bf12af360596f674ef3
Anderson Silva <andersonsilvadev01@gmail.com>
2024-09-23 17:30:52 -0300

    funcionalidade pre-cadastro

--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -13,24 +13,48 @@ describe('Funcionalidade pré-cadastro', () => {
         cy.get(':nth-child(4) > .button').click()
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('be.visible')
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > [href="http://lojaebac.ebaconline.art.br/minha-conta/edit-account/"]').click()
-        cy.get('#account_first_name').clear().type(faker.person.firstName(faker.internet.email()))
+        cy.get('#account_first_name').clear().type(faker.person.firstName())
         cy.get('#account_last_name').clear().type(faker.person.lastName())
         cy.get('.woocommerce-Button').click()
         cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso')
     })  
     it('Deve completar o pré-cadastro com sucesso usando Comandos customizados', () => {
-        
+        cy.preCadastro(faker.internet.email(), faker.internet.password(), faker.person.firstName(), faker.person.lastName())
     });
     
     it('Deve exibir mensagem de erro ao não inserir o sobrenome', () => {
-        
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type(faker.internet.password())
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('be.visible')
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > [href="http://lojaebac.ebaconline.art.br/minha-conta/edit-account/"]').click()
+        cy.get('#account_first_name').clear().type(faker.person.firstName())
+        cy.get('#account_last_name').clear()
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-error').should('contain', 'Sobrenome é um campo obrigatório.')
     });
     
-    it('Deve exibir mensagem de erro ao não inserir o nome', () => {
-        
+    it.only('Deve exibir mensagem de erro ao não inserir o nome', () => {
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type(faker.internet.password())
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('be.visible')
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > [href="http://lojaebac.ebaconline.art.br/minha-conta/edit-account/"]').click()
+        cy.get('#account_first_name').clear()
+        cy.get('#account_last_name').clear().type(faker.person.lastName())
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-error').should('contain', 'Nome é um campo obrigatório.')
     });
 
     it('Deve exibir mensagem de erro se não inserir o nome e sobrenome', () => {
-        
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type(faker.internet.password())
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('be.visible')
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > [href="http://lojaebac.ebaconline.art.br/minha-conta/edit-account/"]').click()
+        cy.get('#account_first_name').clear()
+        cy.get('#account_last_name').clear()
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-error').should('contain', 'Nome é um campo obrigatório.')
     });
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,6 +24,21 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
+import { faker } from '@faker-js/faker'
+
+Cypress.Commands.add('preCadastro', (email, senha, nome, sobrenome) => {
+  cy.get('#reg_email').type(email)
+  cy.get('#reg_password').type(senha)
+  cy.get(':nth-child(4) > .button').click()
+  cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('be.visible')
+  cy.get('.woocommerce-MyAccount-navigation-link--edit-account > [href="http://lojaebac.ebaconline.art.br/minha-conta/edit-account/"]').click()
+  cy.get('#account_first_name').clear().type(nome)
+  cy.get('#account_last_name').clear().type(sobrenome)
+  cy.get('.woocommerce-Button').click()
+  cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso')
+  });
+
+
 Cypress.Commands.add('login', (email, senha) => {
     cy.get('#username').type(email)
     cy.get('#password').type(senha,{log: false})
