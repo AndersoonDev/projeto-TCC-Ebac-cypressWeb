b33a262feca0b1c8e34087da540785785842cea4
Anderson Silva <andersonsilvadev01@gmail.com>
2024-09-23 19:32:11 -0300

    funcionalidade enderecos

---
+++ cypress/e2e/04_endereco.cy.js
@@ -0,0 +1,34 @@
+/// <reference types='cypress' />
+
+describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
+    beforeEach(() => {
+        cy.visit('minha-conta');
+        cy.login('aluno_ebac@teste.com', 'teste@teste.com');
+    });
+
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
+
+    });
+
+    it('Deve fazer cadastro de faturamento com sucesso - usando arquivo de dados', () => {
+        
+    });
+
+    it('Deve fazer cadastro de endereço de entrega com sucesso - usando arquivo de dados', () => {
+        
+    });
+
+    it('', () => {
+        
+    });
+
+    it('', () => {
+        
+    });
+
+    it('', () => {
+        
+    });
+
+    
+});
